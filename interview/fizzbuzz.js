684fc11bce4799d8d3f5a3bb4cc17c42a2fd3dbb
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-01-31 09:17:16 +0100

    fizzBuzz

--- interview/fizzbuzz.js
+++ interview/fizzbuzz.js
@@ -1,11 +1,38 @@
-const fizzBuzz = () => {
-    console.log('fizz');
+// const fizzBuzz = (N) => {
+//   let arr = [];
+//   for (let i = 1; i <= N; i++) {
+//     let newVariable = '';
+//     if (i % 3 == 0) newVariable += 'Fizz';
+//     if (i % 5 == 0) newVariable += 'Buzz';
+//     if (newVariable == '') newVariable = i;
+//     arr.push(newVariable);
+//   }
+//   console.log(arr);
+//   return arr;
+// };
+
+// fizzBuzz(5);
+
+const fizzBuzz1 = (N) => {
+  const lookup = {
+    3: 'Fizz',
+    5: 'Buzz'
+  };
+  let arr = [];
+  for(i = 1; i <= N; i++) {
+    let newVariable = '';
+    for(let key in lookup) {
+        if (i % key == 0) {
+            newVariable += lookup[key];
+        }
+    }
+    if (newVariable == '') {
+        newVariable = i;
+    }
+    arr.push(newVariable)
+  }
+  return arr;
 };
 
-const hiThere = () => {
-    console.log('hi');
-}
-
-
-
-module.exports = fizzBuzz;
+// module.exports = fizzBuzz;
+module.exports = fizzBuzz1;
--- interview/fizzbuzz.test.js
+++ interview/fizzbuzz.test.js
@@ -1,2 +1,44 @@
-const fizzbuzz = require('./fizzbuzz');
+const fizzBuzz = require('./fizzbuzz');
 
+describe('fizzBuzz tests', () => {
+    test('when given a number N, it returns an array from 1 to N', () => {
+        // arange
+        let N = 2;
+        let expected = [1, 2];
+
+        // act
+        let actual = fizzBuzz(N);
+
+        // assert
+        expect(actual).toEqual(expected);
+
+    });
+
+    test('if returned number in array divisible by 3, print "Fizz"', () => {
+        let N = 3;
+        let expected = [1, 2, 'Fizz'];
+        let actual = fizzBuzz(N);
+        expect(actual).toEqual(expected);
+    });
+
+    test('if returned number in array divisible by 3, print "Fizz"', () => {
+        let N = 4;
+        let expected = [1, 2, 'Fizz', 4];
+        let actual = fizzBuzz(N);
+        expect(actual).toEqual(expected);
+    });
+
+    test('if returned number in array divisible by 5, print "Buzz"', () => {
+        let N = 6;
+        let expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz'];
+        let actual = fizzBuzz(N);
+        expect(actual).toEqual(expected);
+    });
+
+    test('if returned number in array divisible by 15, print "FizzBuzz"', () => {
+        let N = 18;
+        let expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz'];
+        let actual = fizzBuzz(N);
+        expect(actual).toEqual(expected);
+    });
+})
