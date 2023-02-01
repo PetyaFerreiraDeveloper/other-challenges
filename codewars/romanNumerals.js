2cd61c6c8ee42207f1d3606cc580c30d3fd15863
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-01 15:01:14 +0100

    roman numerals

--- codewars/romanNumerals.js
+++ codewars/romanNumerals.js
@@ -1,5 +1,29 @@
-const romanNumerals = () => {
-    return true;
+const romanNumerals = (roman) => {
+    const numerals = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
+    };
+    let romanArray = roman.split('');
+    
+    let sum = 0;
+    for(let i = 0; i < romanArray.length; i++) {
+        if(numerals[romanArray[i]] < numerals[romanArray[i + 1]]) {
+            sum += (numerals[romanArray[i + 1]] - numerals[romanArray[i]]);
+            i++;
+        } else {
+            sum += numerals[romanArray[i]];
+        }
+    }
+    return sum;
+    
 };
+
+romanNumerals('I');
+
 
 module.exports = romanNumerals;
--- codewars/romanNumerals.test.js
+++ codewars/romanNumerals.test.js
@@ -1,5 +1,23 @@
 const romanNumerals = require('./romanNumerals');
 
-test('it should return true', () => {
-    expect(romanNumerals(true)).toEqual(true);
+test('it should return 1', () => {
+    expect(romanNumerals('I')).toEqual(1);
 });
+
+test('it should return 2', () => {
+    expect(romanNumerals('II')).toEqual(2);
+});
+
+test('it should return 3', () => {
+    expect(romanNumerals('III')).toEqual(3);
+});
+
+test('it should return 4', () => {
+    expect(romanNumerals('IV')).toEqual(4);
+});
+
+test('it should return 2567', () => {
+    expect(romanNumerals('MMDLXVII')).toEqual(2567);
+});
+
+
