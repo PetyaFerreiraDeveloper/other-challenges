dad4d017df38be98949a00289be949072ab9d0d9
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-05 15:30:29 +0100

    fix

--- codewars/7kyu/especiallyJoyfulNumber.test.js
+++ codewars/7kyu/especiallyJoyfulNumber.test.js
@@ -1,7 +1,7 @@
 const joyfulNumber = require('./especiallyJoyfulNumber');
 
 test('it works with 1', () => {
-    expect(joyfulNumber(1)).toBe(false);
+    expect(joyfulNumber(1)).toBe(true);
 });
 
 test('it works with 2', () => {
